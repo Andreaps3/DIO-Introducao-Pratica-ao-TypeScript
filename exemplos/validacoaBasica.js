c4a8890fcd692375d03a9a977ee622d8c704557b
Andrea Santos <andrea.ps03@gmail.com>
2022-08-05 22:37:50 -0300

    first commit

---
+++ exemplos/validacoaBasica.js
@@ -0,0 +1,16 @@
+/*let button = documento.getElementById('button');
+let input1 = documento.getElementById('input1');
+let input2 = documento.getElementById('input2');
+
+function somarNumeros(numero1, numero2){
+    if(typeof numero1 ==='number' && typeof numero2 === 'number'){
+        return numero1 + numero2
+    }else{
+        return Number(numero1) + (numero2)
+    }
+    
+}
+
+button.addEventListener('click', () => {
+    console.log(somarNumeros(input1.value, input2.value));
+})*/
